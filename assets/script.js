4c2c2a04bdd40e99834fcf973320b3e0c8fe68ef
fredgaloppin <76943745+fredgaloppin@users.noreply.github.com>
2021-02-11 11:38:15 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 
 let can_play = true;
-let words = new Array("WOOD", "TIMBER", "TURING", "MOBILE", "MERGE",
+let words = new Array("WOOD", "TIMBER", "MACHINE", "MOBILE", "MERGE",
  "CODES");
 
 let to_guess = "";
@@ -20,80 +20,66 @@ let wrong_guesses = 0;
 
 
 //-----end attempt-------------------------------//
-function selectLetter(l)
-{
-if (can_play == false)
-{
-return;
-}
- 
-if (used_letters.indexOf(l) != -1)
-{
-return;
-}
- 
-
- 
-if (to_guess.indexOf(l) != -1)
-{
+function selectLetter(l){
+	if (can_play == false){
+		return;
+	}
+	if (used_letters.indexOf(l) != -1){
+		return;
+	}
+	if (to_guess.indexOf(l) != -1){
  // correct letter guess
-pos = 0;
-temp_mask = display_word;
+		pos = 0;
+		temp_mask = display_word;
  
-while (to_guess.indexOf(l, pos) != -1)
-{
-pos = to_guess.indexOf(l, pos);			
-end = pos + 1;
+		while (to_guess.indexOf(l, pos) != -1){
+			pos = to_guess.indexOf(l, pos);			
+			end = pos + 1;
+			
+			start_text = temp_mask.substring(0, pos);
+			end_text = temp_mask.substring(end, temp_mask.length);
+			
+			temp_mask = start_text + l + end_text;
+			pos = end;
+		}
  
-start_text = temp_mask.substring(0, pos);
-end_text = temp_mask.substring(end, temp_mask.length);
+	display_word = temp_mask;
+	document.getElementById("displayWord").innerHTML = display_word;
  
-temp_mask = start_text + l + end_text;
-pos = end;
-}
- 
-display_word = temp_mask;
-document.game.displayWord.value = display_word;
- 
-if (display_word.indexOf("*") == -1)
-{
+		if (display_word.indexOf("*") == -1){
 // won
-document.getElementById("message").innerHTML = "Well done, you're free!";
-document.getElementById("attempts").innerHTML = "Let's party!";
-if (wrong_guesses == 0) {
-	document.getElementById("attempts").innerHTML = "A perfect, let's party! ";
-}
-can_play = false;
-}
-}
-else
-{
+			document.getElementById("message").innerHTML = "Well done, you're free!";
+			document.getElementById("attempts").innerHTML = "Let's party!";
+			if (wrong_guesses == 0) {
+				document.getElementById("attempts").innerHTML = "A perfect, let's party! ";
+			}
+		can_play = false;
+		}
+	} else {
 // incorrect letter guess
-used_letters += l;
-document.game.usedLetters.value = used_letters;
-wrong_guesses += 1;
-eval("document.hm.src=\"assets/hang" + wrong_guesses + ".png\"");
-document.getElementById("attempts").innerHTML = 10-wrong_guesses+" wrong guesse(s) left";
-if (wrong_guesses == 10)
-{
+	used_letters += l;
+	document.getElementById("usedLetters").innerHTML = used_letters;
+	wrong_guesses += 1;
+	eval("document.hm.src=\"assets/hang" + wrong_guesses + ".png\"");
+	document.getElementById("attempts").innerHTML = 10-wrong_guesses+" wrong guesse(s) left";
+		if (wrong_guesses == 10){
 // lost
-document.getElementById("message").innerHTML = "Poor lad, Far West is so unfair! Whatever the last one at the bar pay!";
-document.getElementById("attempts").innerHTML = "";
-document.getElementById("displayWord").innerHTML = to_guess;
-can_play = false;
-}
-}
+			document.getElementById("message").innerHTML = "Poor lad, Far West is so unfair! Whatever the last one at the bar pay!";
+			document.getElementById("attempts").innerHTML = "";
+			document.getElementById("displayWord").innerHTML = to_guess;
+			can_play = false;
+		}
+	}
 }
  
-function reset()
-{
-document.getElementById("message").innerHTML = "";
-document.getElementById("attempts").innerHTML = "";
-selectWord();
-document.game.usedLetters.value = "";
-used_letters = "";
-wrong_guesses = 0;
-document.hm.src="assets/hang0.png";
+function reset(){
+	document.getElementById("message").innerHTML = "";
+	document.getElementById("attempts").innerHTML = "";
+	selectWord();
+	used_letters = "";
+	document.getElementById("usedLetters").innerHTML = used_letters;
+	wrong_guesses = 0;
+	document.hm.src="assets/hang0.png";
 } 
 function selectWord()
 {
@@ -103,20 +89,18 @@ random_number = Math.round(Math.random() * (words.length - 1));
 to_guess = words[random_number];
 // display masked word
 masked_word = createMask(to_guess);
-document.game.displayWord.value = masked_word;
+document.getElementById("displayWord").innerHTML = masked_word;
 display_word = masked_word;
 }
  
-function createMask(m)
-{
-mask = "";
-word_lenght = m.length;
+function createMask(m){
+	mask = "";
+	word_lenght = m.length;
  
-for (i = 0; i < word_lenght; i ++)
-{
-mask += "*";
-}
-return mask;
+	for (i = 0; i < word_lenght; i ++){
+		mask += "*";
+	}
+	return mask;
 }
 //-----------dictionnary-------------//
 // let vocab = require("fs");
