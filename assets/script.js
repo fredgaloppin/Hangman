fdde94f5a327ebafc08164ca5ed72be22ccd7f12
fredgaloppin <76943745+fredgaloppin@users.noreply.github.com>
2021-02-14 14:18:12 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -7,19 +7,9 @@ let to_guess = "";
 let display_word = "";
 let used_letters = "";
 let wrong_guesses = 0;
-//--------------attempt-------------------//
+//--------------end primary declarations-------------------//
 
-// let allText =[];
-// let allTextLines = [];
-// let Lines = [];
-
-// let txtFile = new XMLHttpRequest();
-// txtFile.open("GET", "3000+ Common English Words", true);
-// allText = txtFile.responseText;
-// console.log(allText);
-
-
-//-----end attempt-------------------------------//
+//---function on event -------//
 function selectLetter(l){
 	if (can_play == false){
 		return;
@@ -27,8 +17,9 @@ function selectLetter(l){
 	if (used_letters.indexOf(l) != -1){
 		return;
 	}
+	document.getElementById(l).style.opacity = 0.5;
 	if (to_guess.indexOf(l) != -1){
- // correct letter guess
+ // ---correct letter guess -----------------//
 		pos = 0;
 		temp_mask = display_word;
  
@@ -47,7 +38,7 @@ function selectLetter(l){
 	document.getElementById("displayWord").innerHTML = display_word;
  
 		if (display_word.indexOf("*") == -1){
-// won
+// -------won ----------------------------//
 			document.getElementById("message").innerHTML = "Well done, you're free!";
 			document.getElementById("attempts").innerHTML = "Let's party!";
 			if (wrong_guesses == 0) {
@@ -56,14 +47,14 @@ function selectLetter(l){
 		can_play = false;
 		}
 	} else {
-// incorrect letter guess
+// -------incorrect letter guess----//
 	used_letters += l;
 	document.getElementById("usedLetters").innerHTML = used_letters;
 	wrong_guesses += 1;
-	eval("document.hm.src=\"assets/hang" + wrong_guesses + ".png\"");
+	document.getElementById("hm").src = "assets/hang" + wrong_guesses + ".png";
 	document.getElementById("attempts").innerHTML = 10-wrong_guesses+" wrong guesse(s) left";
 		if (wrong_guesses == 10){
-// lost
+//----- lost-------------------//
 			document.getElementById("message").innerHTML = "Poor lad, Far West is so unfair! Whatever, the last one at the saloon buy a round of grogs!";
 			document.getElementById("attempts").innerHTML = "";
 			document.getElementById("displayWord").innerHTML = to_guess;
@@ -71,7 +62,7 @@ function selectLetter(l){
 		}
 	}
 }
- 
+//-------- function ---- reset---+ call selectWord-------//
 function reset(){
 	document.getElementById("message").innerHTML = "";
 	document.getElementById("attempts").innerHTML = "";
@@ -79,20 +70,21 @@ function reset(){
 	used_letters = "";
 	document.getElementById("usedLetters").innerHTML = used_letters;
 	wrong_guesses = 0;
-	document.hm.src="assets/hang0.png";
+	document.getElementById("hm").src="assets/hang0.png";
 } 
+//-----random word + call createMask + display mask---//
 function selectWord()
 {
 can_play = true;
 
 random_number = Math.round(Math.random() * (words.length - 1));
 to_guess = words[random_number];
-// display masked word
+// -----------display masked word-----------------------------//
 masked_word = createMask(to_guess);
 document.getElementById("displayWord").innerHTML = masked_word;
 display_word = masked_word;
 }
- 
+ // --- funtcion createMask------//
 function createMask(m){
 	mask = "";
 	word_lenght = m.length;
@@ -102,29 +94,4 @@ function createMask(m){
 	}
 	return mask;
 }
-//-----------dictionnary-------------//
-// let vocab = require("fs");
-// fs.readFile("./EnglishWords.txt", function(text){
-//     let textByLine = text.split("\n")
-// 	console.log(textByLine);
-// });
-// npm install random-words
 
-// let randomWords = require('random-words');
-// console.log(randomWords());
-
-//------canvas-----------------------//
-
-
-// function draw() {
-// 	var canvas = document.getElementById("hangman");
-// 	if (canvas.getContext) {
-// 	  var ctx = canvas.getContext("2d");
-
-// 	  ctx.fillStyle = 'rgb(200, 0, 0)';
-// 	  ctx.fillRect(10, 10, 50, 50);
-
-// 	  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-// 	  ctx.fillRect(30, 30, 50, 50);
-// 	}
-//   }
